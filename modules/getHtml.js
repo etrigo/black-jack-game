304652b2377a356a56ccddfa5dc167f1feee2bee
etrigo1 <bjorn.sjostedt@gmail.com>
2024-03-22 20:13:10 +0100

    Remove dist folder

---
+++ modules/getHtml.js
@@ -0,0 +1,14 @@
+import { theGame } from './gameObject.js';
+
+// create html
+let getHtml = (x) => {
+  theGame[x].displayCards = ``;
+  for (let i = 0; i < theGame[x].cards.length; i++) {
+    theGame[x].displayCards += `
+          <img src="/img/${theGame[x].cards[i].cardNum}.png" alt="Cards" class="card">
+          `;
+  }
+  return theGame[x].displayCards;
+};
+
+export { getHtml };
